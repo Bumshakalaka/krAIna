c4b2933bdbc58eeaa7d71595fd1a4d687de1e2f5
BUMshakalaka <tyzio.t@gmail.com>
2024-05-13 15:48:03 +0200

    feat(llm): Enhance model mapping and AzureAI detection
    Enhance the LLM handling by introducing model mapping from OpenAI to AzureAI and improving AzureAI detection mechanism.

--- libs/llm.py
+++ libs/llm.py
@@ -5,7 +5,29 @@ from typing import Union
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 
 
-def chat_llm(*args, **kwargs) -> Union[ChatOpenAI, AzureChatOpenAI]:
+def map_model(model: str) -> str:
+    """
+    Map OpenAI model names to AzureAI
+
+    :param model: openAI model name
+    :return: AzureAI model name
+    """
+    map_models = {"gpt-4-turbo": "gpt-4-turbo-128k", "gpt-3.5-turbo": "gpt-35-turbo"}
+    if isAzureAI():
+        return map_models.get(model, model)
+    else:
+        return model
+
+
+def isAzureAI() -> bool:
+    """Is Azure LLM in use?"""
+    return bool(
+        os.environ.get("AZURE_OPENAI_API_KEY")
+        and os.environ.get("AZURE_OPENAI_ENDPOINT")
+    )
+
+
+def chat_llm(**kwargs) -> Union[ChatOpenAI, AzureChatOpenAI]:
     """
     Select and return on of the LLM to use.
 
@@ -13,9 +35,8 @@ def chat_llm(*args, **kwargs) -> Union[ChatOpenAI, AzureChatOpenAI]:
     Otherwise, initialize ChatOpenAI.
 
     """
-    if os.environ.get("AZURE_OPENAI_API_KEY") and os.environ.get(
-        "AZURE_OPENAI_ENDPOINT"
-    ):
-        return AzureChatOpenAI(*args, **kwargs)
+    if isAzureAI():
+        kwargs["model"] = map_model(kwargs["model"])
+        return AzureChatOpenAI(**kwargs)
     else:
-        return ChatOpenAI(*args, **kwargs)
+        return ChatOpenAI(**kwargs)
