f27db2dbce2e0a693dbac2e07aee039a4611f585
BUMshakalaka <tyzio.t@gmail.com>
2024-05-13 10:57:56 +0200

    feat(assistant): Support chat history in BaseAssistant run method
    Add parameter `hist` to run method to handle chat history.

--- assistants/assistant.py
+++ assistants/assistant.py
@@ -1,8 +1,9 @@
 """Base skill class."""
 import logging
 from dataclasses import dataclass
+from typing import List
 
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 
 logger = logging.getLogger(__name__)
@@ -32,11 +33,12 @@ class BaseAssistant:
         if not cls.__name__.startswith("_"):
             SPECIALIZED_ASSISTANT[cls.__name__] = cls
 
-    def run(self, query: str, /, **kwargs):
+    def run(self, query: str, hist: List = None, /, **kwargs):
         """
         Run the skill with user query.
 
         :param query: text which is passed as Human text to LLM chat.
+        :param hist: Chat history
         :param kwargs: additional key-value pairs to substitute in System prompt
         :return:
         """
@@ -48,9 +50,10 @@ class BaseAssistant:
         prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", self.prompt),
+                MessagesPlaceholder("hist") if hist else ("human", ""),
                 ("human", "{text}"),
             ]
         )
-        ret = chat.invoke(prompt.format_prompt(text=query, **kwargs))
+        ret = chat.invoke(prompt.format_prompt(text=query, hist=hist, **kwargs))
         logger.info(f"{self.name}: ret={ret}")
         return ret.content
