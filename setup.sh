6f8779b53a66b215865564b41cba905a0093fc51
BUMshakalaka <tyzio.t@gmail.com>
2024-07-06 20:41:04 +0200

    feat(setup.sh): Add error handling with trap
    Add a function to trap errors during script execution and exit with an error message.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+function err_trap () {
+    echo "$0: line $1: exit status of last command: $2"
+    exit 1
+}
+
+set -E
+trap 'err_trap ${LINENO} ${$?}' ERR
+
 ASK=$1
 python3 -m venv .venv
 source .venv/bin/activate
