75b6709463e7e2bdd926c766c1903e8eac2220aa
BUMshakalaka <tyzio.t@gmail.com>
2025-07-16 14:39:02 +0200

    fix(kraina_app): update dotenv loading mechanism
    - Replace `find_dotenv()` with a direct reference to `ENV_FILE` for loading environment variables in `kraina_app.py`.
    - This change simplifies the dotenv loading process and ensures the correct environment file is used.

--- app/kraina_app.py
+++ app/kraina_app.py
@@ -1,6 +1,6 @@
 import sys
 
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 from ipyc import IPyCHost
 
 from kraina.libs.ipc.host import AppHost
@@ -14,9 +14,10 @@ except OSError:
     if getattr(sys, "frozen", False):
         pyi_splash.close()
 else:
+    from kraina.libs.paths import ENV_FILE
     from kraina_chat.main import App
 
-    load_dotenv(find_dotenv())
+    load_dotenv(ENV_FILE)
     app = App()
     AppHost(app).start()
     app.deiconify()
