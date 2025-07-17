2685a8067199ac78f4ca32836c6a1e1f0de0c2ec
BUMshakalaka <tyzio.t@gmail.com>
2025-07-17 15:53:03 +0200

    remove outdated

--- app/chat.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-if "%1"=="" (
-    cd %~dp0 & .venv\scripts\activate.bat & start "" pythonw chat.py
-) else (
-    cd %~dp0 & .venv\scripts\activate.bat & python chat.py %* & deactivate
-)
